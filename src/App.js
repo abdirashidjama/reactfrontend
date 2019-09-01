48b2190dd3cf08997cdb61e9af6bb8c4fe0a0b9b
Rashid <abdirashidjama@cmail.carleton.ca>
2019-09-01 15:28:40 -0400

    Coding: added Routers for new student from and showing all students

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import StudentForm from './StudentForm';
 import ShowStudents from './ShowStudents';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 
 class App extends Component {
@@ -21,14 +22,30 @@ class App extends Component {
 	}
 	render(){
 		const students= this.state.students;
+		const form = <StudentForm />
+		const showStudents = <ShowStudents students= {this.state.students} />
 		return (
-			<div className="App">
-				<h>
-					Coach Space
-				</h>
-				<StudentForm />
-				<ShowStudents students={this.state.students} />
-			</div>
+				<div style={{display:"flex", flexDirection:"column"}} className="App">
+					<h>
+						Coach Space
+						</h>
+						<input />
+						<Link to ="/allStudents">
+							<span className="card" style={{display:"block"}}>
+								<h2>View all students</h2>
+								<img src="https://img.icons8.com/ios/50/000000/crowd.png" />
+							</span>
+						</Link>
+						<Link to ="/newStudent">
+							<span className="card" style={{display:"block"}}>
+								<h2>Add new Student</h2>
+								<img src="https://img.icons8.com/ios/50/000000/add-user-male.png"/>
+							</span>
+						</Link>
+						<footer style={{alignContent: "flex-end", marginBottom: 0}}>
+						<a href="https://icons8.com/icon/59220/crowd">icons by Icons8</a>
+						</footer>
+				</div>
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import StudentForm from './StudentForm';
+import ShowStudents from './ShowStudents';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing =(
+	<Router>
+		<div>
+			<Route exact path="/" component={App}/>
+			<Route path="/newStudent" component={StudentForm} />
+			<Route path="/allStudents" component={ShowStudents}/>
+		</div>
+	</Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
