390a21e4a2f83f6ec458ed242e8eea24d9e9efed
Rashid <abdirashidjama@cmail.carleton.ca>
2019-09-04 14:42:01 -0400

    Code: addedcode to get image file and post to firebase cloud storage. The generated image link is put into mongodb database.

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import   firebase from '../firebase-config';
 
 class StudentForm extends Component{
 	constructor(props){
 		super(props)
-		
 		this.state =
 		{
 			firstName: "",
@@ -11,11 +11,18 @@ class StudentForm extends Component{
 			email: "",
 			contactNumber: "",
 			membershipExpiry: "",
-			hours: ""
+			hours: "",
+			studentImage: "",
+			imageUrl:""
+
+		}
+		this.setRef = ref => {
+			this.file = ref
 		}
 		this.handleChange= this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
+
 	handleChange(event){
 		const target = event.target;
 		const name = target.name;
@@ -31,24 +38,61 @@ class StudentForm extends Component{
 		}
 		alert('Form submitted for ' + '\n' + listState);
 		event.preventDefault();
-		var url = 'http://localhost:3001/students/';
-		const data = {
-			firstname : this.state.firstName,
-			lastname : this.state.lastName,
-			email: this.state.email,
-			number: this.state.number,
-			memexpdate: this.state.membershipExpiry,
-			hours: this.state.hours
-		}
-		fetch(url, {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: {
-				'Content-Type': 'application/json'
+		
+
+		//get file
+		const file = this.file.files[0];
+		//create storage ref
+		const storageRef = firebase.storage().ref();
+		var metadata = {
+			contentType: 'image/jpeg'
+		};
+
+		var task = storageRef.child(this.file.files[0].name).put(file, metadata);
+
+		var urlImage = "";
+
+		task.on(firebase.storage.TaskEvent.STATE_CHANGED,
+			(snapshot)=>{
+
+			}, (error)=>{
+
+			}, ()=>{
+				task.snapshot.ref.getDownloadURL().then((urlDownload)=>{
+					this.setState({
+						imageUrl: urlDownload
+					}, 
+					
+					()=> {
+						console.log(this.state.imageUrl);
+						var url = 'http://localhost:3001/students/';
+						const data = {
+							firstname : this.state.firstName,
+							lastname : this.state.lastName,
+							email: this.state.email,
+							number: this.state.number,
+							memexpdate: this.state.membershipExpiry,
+							hours: this.state.hours,
+							studentImage: this.state.imageUrl
+						}
+						fetch(url, {
+							method: 'POST',
+							body: JSON.stringify(data),
+							headers: {
+								'Content-Type': 'application/json'
+							}
+						})
+						.then(res => res.json())
+						.then(response=>console.log('Success:', JSON.stringify(response)))
+						.catch(error => console.error('Error:', error));
+					}
+					);
+				});
 			}
-		}).then(res => res.json())
-		.then(response=>console.log('Success:', JSON.stringify(response)))
-		.catch(error => console.error('Error:', error));
+		);
+
+
+
 	}
 	render(){
 		const styled = {
@@ -80,7 +124,6 @@ class StudentForm extends Component{
 				name: "hours",
 				type: "number"
 			}
-			
 		]
 		const listItems = formData.map(data =>
 			<label>
@@ -89,16 +132,18 @@ class StudentForm extends Component{
 				value={this.state[data.name.replace(/\s/g,'')]} 
 				name={data.name.replace(/\s/g,'')}
 				onChange={this.handleChange} 
+				id={data.name}
 				/>
 			</label>
 		)
 		return (
-			<div className="StudentForm">
+			<div className="StudentForm">	
 				<h1>Add Student</h1>
 				<form onSubmit={this.handleSubmit} style={styled}>
 					{
 						listItems
 					}
+					<input type="file" ref={this.setRef} />
 					<input type="submit" value="Submit" />
 				</form>
 			</div>
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 var config = {
-		apiKey: ,
-		authDomain: ,
-		databaseURL: ,
-		projectId:,
-		storageBucket:,
-		messagingSenderId:		
+
+		apiKey: "AIzaSyDXoUwAL_dBs8rmqnoYCIPXGlYj1MNmSWA",
+	    authDomain: "pic-storage-5c1d7.firebaseapp.com",
+	    databaseURL: "https://pic-storage-5c1d7.firebaseio.com",
+	    projectId: "pic-storage-5c1d7",
+	    storageBucket: "pic-storage-5c1d7.appspot.com",
+	    messagingSenderId: "512330873698",
+	    appId: "1:512330873698:web:6896eb3ce0c981c3"
 };
 
 firebase.initializeApp(config);
